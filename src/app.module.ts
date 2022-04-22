1def367c89184ba836c518c5268314b59714f8a5
Emna Cheniour <emnacheniour@insat.u-carthage.tn>
2022-04-22 10:21:32 +0100

    Database security

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service'; 
-import { MongooseModule } from '@nestjs/mongoose';//to make a db connection 
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose'; //to make a db connection
 import { ConfigModule } from '@nestjs/config';
-import { JwtModule } from '@nestjs/jwt'; 
+import { JwtModule } from '@nestjs/jwt';
 import { HostModule } from './host-auth/host/host.module';
 import { UserModule } from './client-auth/user/user.module';
 import { HostAuthController } from './host-auth/auth/auth.controller';
 import { UserAuthController } from './client-auth/auth/auth.controller';
 import { HostAuthModule } from './host-auth/auth/auth.module';
 import { UserAuthModule } from './client-auth/auth/auth.module';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+
+const DB = process.env.DB_connection;
+
 @Module({
-  imports: [  
+  imports: [
     ConfigModule.forRoot({
-      isGlobal:true
+      isGlobal: true,
     }),
-    UserModule,UserAuthModule,
-    MongooseModule.forRoot('mongodb+srv://salma:salma@cluster0.eeaxx.mongodb.net/plandoDatabase?retryWrites=true&w=majority'), 
-    HostAuthModule, HostModule],
-  controllers: [AppController, HostAuthController,UserAuthController],
-  providers: [AppService ],
+    UserModule,
+    UserAuthModule,
+    MongooseModule.forRoot(DB),
+    HostAuthModule,
+    HostModule,
+  ],
+  controllers: [AppController, HostAuthController, UserAuthController],
+  providers: [AppService],
 })
 export class AppModule {}
