109ba970e5c08243a84286e6903a35a28a3804ff
unknown <yahyaouisalma14@gmail.com>
2022-05-07 00:21:41 +0100

    app_port

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose'; //to make a db connection
-import { ConfigModule } from '@nestjs/config';
-import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config'; 
 import { HostModule } from './host-auth/host/host.module';
 import { UserModule } from './client-auth/user/user.module';
 import { HostAuthController } from './host-auth/auth/auth.controller';
@@ -11,6 +10,8 @@ import { UserAuthController } from './client-auth/auth/auth.controller';
 import { HostAuthModule } from './host-auth/auth/auth.module';
 import { UserAuthModule } from './client-auth/auth/auth.module';
 import { EstateModule } from './estate/estate.module';
+import { APP_PIPE } from '@nestjs/core';
+import { ValidationPipe } from './validation.pipe';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config();
 
