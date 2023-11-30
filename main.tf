9628bc21df36d2b6d0ca9777c4942e289e3a1242
asmaSmida <79024325+asmaSmida@users.noreply.github.com>
2023-11-30 12:01:09 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,14 +7,30 @@ terraform {
 }
 
 provider "docker" {
-  host = "tcp://localhost:2375"
+  # Connect to the Docker daemon using the default Unix socket
+}
+
+resource "docker_network" "app_network" {
+  name = "nestjs-mongo-network"
 }
 
 resource "docker_container" "mongo" {
-  image = "mongo:latest"
-  name  = "mongo-container"
+  image  = "mongo:latest"
+  name   = "mongo-container"
+  networks = [docker_network.app_network.name]
   ports {
     internal = 27017
-    external = 27017
   }
 }
+
+resource "docker_container" "nestjs_app" {
+  image  = "asmasmida13/dockertp:planDoImageBackend"
+  name   = "nestjs-container"
+  networks = [docker_network.app_network.name]
+  ports {
+    internal = 3000
+  }
+  depends_on = [docker_container.mongo]
+}
+
+# Additional Resources and Configurations...
