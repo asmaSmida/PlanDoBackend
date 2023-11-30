e60bc2bc5cee1844a90ca030859e26198426bb57
asmaSmida <79024325+asmaSmida@users.noreply.github.com>
2023-11-30 21:27:42 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,13 +2,12 @@ terraform {
   required_providers {
     docker = {
       source = "kreuzwerker/docker"
+      version = "~> 3.0.1"
     }
   }
 }
 
-provider "docker" {
-  host = "unix:///var/run/docker.sock"
-}
+provider "docker" {}
 
 resource "docker_network" "app_network" {
   name = "nestjs-mongo-network"
