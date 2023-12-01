0dff67576c7942dd39de0ada0a1a43c408799b12
asmaSmida <79024325+asmaSmida@users.noreply.github.com>
2023-12-01 14:15:15 +0100

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,8 +0,0 @@
-# outputs.tf
-output "nestjs_app_url" {
-  value = azurerm_container_group.nestjs_mongo_aci.fqdn
-}
-
-output "mongo_db_url" {
-  value = "${azurerm_container_group.nestjs_mongo_aci.ip_address}:${azurerm_container_group.nestjs_mongo_aci.container.1.ports[0].external}"
-}
